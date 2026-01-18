3bf73a6b1d1b80db3e1ec748453c9248ce3ffd0c
Martin Loza <martin.loza.lopez@gmail.com>
2026-01-19 07:56:39 +0900

    Add analysis of transcription type
    Sense vs antisense

---
+++ 04_Figure_window_size_and_GO/Analysis_transcription_type/03_AME.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This script will convert BED files to FASTA and find enriched motifs using a control
+# Since we are using a docker version of meme, we need to make sure we are in the right directory. 
+# The Docker's container should be initialized in the following directory:
+# /mnt/c/Users/Marti/Documents/Projects/lncRNA_TF_pairs_analysis
+
+input_dir="04_Figure_window_size_and_GO/Analysis_transcription_type/Results/"
+# output_dir="04_Figure_window_size_and_GO/Analysis_transcription_type//Results/AME/Sense_vs_Antisense/"
+output_dir="04_Figure_window_size_and_GO/Analysis_transcription_type//Results/AME/Antisense_vs_Sense/"
+
+motif_file="Data/JASPAR/JASPAR2022_CORE_vertebrates_redundant_v2.meme"
+genome_file="Data/UCSC_hg38/hg38.fa"
+# query_sample="human_fixed_width_TF_regions_Sense_1000bp_window_260119"
+# control_sample="human_fixed_width_TF_regions_Antisense_1000bp_window_260119"
+control_sample="human_fixed_width_TF_regions_Sense_1000bp_window_260119"
+query_sample="human_fixed_width_TF_regions_Antisense_1000bp_window_260119"
+
+# Make output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+# Convert BED files to FASTA files
+echo "Converting BED files to FASTA..."
+
+# Convert query sample
+if [ -f "${input_dir}${query_sample}.bed" ]; then
+    bed2fasta -s -o "${output_dir}${query_sample}.fa" "${input_dir}${query_sample}.bed" "${genome_file}"
+    echo "Converted ${query_sample}.bed to ${query_sample}.fa"
+else
+    echo "File ${input_dir}${query_sample}.bed does not exist"
+    exit 1
+fi
+
+# Convert control sample
+if [ -f "${input_dir}${control_sample}.bed" ]; then
+    bed2fasta -s -o "${output_dir}${control_sample}.fa" "${input_dir}${control_sample}.bed" "${genome_file}"
+    echo "Converted ${control_sample}.bed to ${control_sample}.fa"
+else
+    echo "File ${input_dir}${control_sample}.bed does not exist"
+    exit 1
+fi
+
+# Run AME motif enrichment analysis
+echo "Running AME motif enrichment analysis..."
+ame --verbose 2 --oc "$output_dir" \
+    --scoring avg \
+    --method fisher \
+    --hit-lo-fraction 0.25 \
+    --evalue-report-threshold 10.0 \
+    --control "${output_dir}${control_sample}.fa" \
+    "${output_dir}${query_sample}.fa" \
+    "${motif_file}"
+
+echo "AME analysis complete. Results in ${output_dir}"
